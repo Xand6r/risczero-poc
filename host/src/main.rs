a098d81c6102fd35b3a2d0c76eb2039504ac5e06
xand6r <shuaibualexander@gmail.com>
2024-04-30 20:53:04 +0100

    refactor implementation to be gpu compatible

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,14 +1,29 @@
-use methods::{HELLO_GUEST_ELF, HELLO_GUEST_ID};
+use methods::{HELLO_GUEST_ELF};
 use risc0_zkvm::{default_prover, ExecutorEnv};
+use serde::{Deserialize, Serialize};
+use tlsn_substrings_verifier::proof::{SessionHeader, SubstringsProof};
+
 pub mod types;
 
 
-fn main() {
-    let env = ExecutorEnv::builder().build().unwrap();
+#[derive(Serialize, Deserialize, Debug)]
+struct ZkParam {
+    header: SessionHeader,
+    substrings: SubstringsProof,
+}
 
+fn main() {
     // Obtain the default prover.
     let prover = default_prover();
 
+    // read in th einputs from json
+    let proof_params = std::fs::read_to_string("./fixtures/zk_params.json").unwrap();
+    let proof_params: ZkParam = serde_json::from_str(proof_params.as_str()).unwrap();
+
+    // pass the input to the guest code
+    let input: (SessionHeader, SubstringsProof) = (proof_params.header, proof_params.substrings);
+    let env = ExecutorEnv::builder().write(&input).unwrap().build().unwrap();
+    
     // Produce a receipt by proving the specified ELF binary.
     let receipt = prover.prove(env, HELLO_GUEST_ELF).unwrap();
 
